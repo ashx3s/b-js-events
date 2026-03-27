6b8a6e6562bd3adce9f81627a33475580c5edef4
Ashlyn Knox <ashx3s@tuta.io>
2026-03-27 14:29:12 -0600

    rendered character list with notes

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 - [innerHTML](https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML)
 - [setHTML](https://developer.mozilla.org/en-US/docs/Web/API/Element/setHTML#browser_compatibility)
 - [Template Literals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
+- [forEach Loop](https://www.freecodecamp.org/news/javascript-foreach-js-array-for-each-example/)
+- [createElement](https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement)
+- [clearing dom nodes](https://www.geeksforgeeks.org/javascript/how-to-clear-the-content-of-a-div-using-javascript/)
--- script.js
+++ script.js
@@ -16,15 +16,33 @@ toggleBtn.addEventListener("click", () => {
 // TASK 2 Second Goal: Reset Button will revert the list back to it's original state
 
 // DOM Selectors for: list, button, form, input
+const characterForm = document.getElementById("character-form");
+const characterInput = document.getElementById("character-input");
+const characterListEl = document.getElementById("character-list");
+const resetBtn = document.getElementById("reset-btn");
 
 // default character list that we can fall back to
+const DEFAULT_CHARACTERS = ["Sauron", "Galadriel", "Fantastic Mr.Fox"];
 
 // working copy of the list that we can manipulate
-
+let characterList = [...DEFAULT_CHARACTERS];
 // function to render the array to the DOM
-
+function renderCharacterList() {
+  // clears the list
+  characterListEl.innerHTML = "";
+  // iterates through everything in the list
+  characterList.forEach((character) => {
+    // create a new li element
+    const li = document.createElement("li");
+    // add the character name to the li element
+    li.textContent = character;
+    // append the li element with the name to the characterListEl
+    characterListEl.appendChild(li);
+  });
+}
 // Event Listener for the form: Submit adds input information to the new list
 
 // Event Listener to reset the list to the original
 
 // run function that renders the list
+renderCharacterList();
